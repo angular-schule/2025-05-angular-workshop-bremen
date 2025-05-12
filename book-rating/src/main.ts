34326f365c05719fff31657131058fee8b1efe61
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-05-12 12:12:13 +0200

    typescript playground

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -4,3 +4,16 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+class Test {
+
+  #zahl: number;
+
+  constructor(zahl: number) {
+    this.#zahl = zahl;
+    console.log('Hallo Bremen! 🫏🐕🐈🐓', this.#zahl.toString());
+  }
+}
+
+const test = new Test(42);
