9248a3b3e4445e159d6fdb9515ef71b762d10670
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-05-14 15:24:48 +0200

    test me!

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,11 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-dashboard',
+  template: '😃',
+})
+export class DummyDashboardComponent {
+}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+    })
+
+  // .overrideComponent(AppComponent, {
+  //   set: { imports: [], schemas: [NO_ERRORS_SCHEMA] }
+  // })
+
+  .overrideComponent(AppComponent, {
+    remove: { imports: [DashboardComponent] },
+    add: { imports: [DummyDashboardComponent] }
+  })
+
+    .compileComponents();
   });
 
   it(`should have the 'Book Rating' title`, () => {
