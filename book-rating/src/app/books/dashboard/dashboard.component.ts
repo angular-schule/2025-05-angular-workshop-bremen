4220c8b02cd7dd96001be0b4fef362e6741dcf92
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-05-14 16:29:47 +0200

    testing with spies

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -38,11 +38,11 @@ export class DashboardComponent {
   }
 
   doRateUp(book: Book) {
-    // const ratedBook = this.br.rateUp(book);
-    const ratedBook = {
-      ...book,
-      rating: Math.min(book.rating + 1, 5)
-    }
+    const ratedBook = this.br.rateUp(book);
+    // const ratedBook = {
+    //   ...book,
+    //   rating: Math.min(book.rating + 1, 5)
+    // }
     this.updateAndSort(ratedBook);
   }
 
