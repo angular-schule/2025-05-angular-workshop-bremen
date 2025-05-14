0985060304cb0a9af25d00edc36f12c1048f934a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-05-14 11:43:12 +0200

    fix: app.component.spec.ts

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -8,22 +8,16 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it(`should have the 'Book Rating' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'book-rating' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-rating');
+    expect(app.title()).toEqual('Book Rating');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, book-rating');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Book Rating');
   });
 });
