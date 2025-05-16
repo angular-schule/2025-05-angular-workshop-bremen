8cc7e09f60d2ebfb6a9a788d4b337517a565ed39
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-05-16 15:25:13 +0200

    rxjs: catchError

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject, input } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BookStoreService } from '../shared/book-store.service';
-import { map, mergeMap } from 'rxjs';
+import { catchError, concatMap, map, mergeMap, of, switchMap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { JsonPipe } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-book-details',
-  imports: [RouterLink, JsonPipe],
+  imports: [RouterLink],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
@@ -21,7 +22,12 @@ export class BookDetailsComponent {
 
   book = toSignal(this.router.paramMap.pipe(
     map(paramMap => paramMap.get('isbn') || ''),
-    mergeMap(isbn => this.bs.getSingle(isbn))
+    switchMap(isbn => this.bs.getSingle(isbn).pipe(
+      catchError((err: HttpErrorResponse) => of({
+        title: 'FEHLER',
+        description: err.message
+      }))
+    ))
   ))
 
 }
