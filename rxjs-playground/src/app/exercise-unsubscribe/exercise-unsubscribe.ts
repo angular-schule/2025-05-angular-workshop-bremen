d052a63c75a97d30454557dcfb5fe6d686ac1576
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-05-16 14:03:36 +0200

    toSignal

--- rxjs-playground/src/app/exercise-unsubscribe/exercise-unsubscribe.ts
+++ rxjs-playground/src/app/exercise-unsubscribe/exercise-unsubscribe.ts
@@ -1,43 +1,16 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil, tap } from 'rxjs';
 
-import { HistoryWindow } from '../shared/history-window/history-window';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   templateUrl: './exercise-unsubscribe.ng.html',
-  imports: [HistoryWindow]
+  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExerciseUnsubscribe {
 
-  logStream$ = new ReplaySubject<string | number>();
-
-  /**
-   * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
-   * Navigiere zurück auf die Startseite und beobachte die Console:
-   * Die Subscription läuft weiter. Wir haben einen klassischen Memory Leak erzeugt ...
-   *
-   * Sorge dafür, dass die Subscription in der Methode ngOnDestroy() beendet wird!
-   * Sie wird beim Buttonklick und beim Wegnavigieren ausgelöst.
-   *
-   * Es gibt noch weitere Wege, das Problem zu lösen ...
-   */
-  constructor() {
-    const interval$ = timer(0, 1000);
-
-    interval$.pipe(
-
-      takeUntilDestroyed()
-
-    ).subscribe({
-      next: e => this.log(e),
-      error: err => this.log('❌ ERROR: ' + err),
-      complete: () => this.log('✅ COMPLETE')
-    });
-  }
-
-  log(msg: string | number) {
-    console.log(msg);
-    this.logStream$.next(msg);
-  }
+  interval$ = toSignal(timer(0, 1000).pipe(
+    //tap(console.log)
+  ), { initialValue: 0 })
 }
