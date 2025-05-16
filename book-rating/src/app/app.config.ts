8b2a84a52db01ac3f77bd2c50601fc969c5f3a5f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-05-16 15:57:45 +0200

    ng add @ngrx/store ng add @ngrx/effects ng add @ngrx/store-devtools ng add @ngrx/schematics

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient()
-  ]
+    provideHttpClient(),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
