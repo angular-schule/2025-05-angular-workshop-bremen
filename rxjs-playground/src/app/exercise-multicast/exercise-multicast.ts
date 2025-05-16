f9a39d304936d92e269b6a2322f72a40015b3da7
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-05-16 12:06:09 +0200

    rxjs: shareReplay

--- rxjs-playground/src/app/exercise-multicast/exercise-multicast.ts
+++ rxjs-playground/src/app/exercise-multicast/exercise-multicast.ts
@@ -1,13 +1,13 @@
 import { Component, OnDestroy, inject, signal } from '@angular/core';
-import { AsyncPipe, DecimalPipe } from '@angular/common';
-import { Subject, BehaviorSubject, ReplaySubject, Observable, share, takeUntil } from 'rxjs';
+import { AsyncPipe, DecimalPipe, JsonPipe } from '@angular/common';
+import { Subject, BehaviorSubject, ReplaySubject, Observable, share, takeUntil, shareReplay } from 'rxjs';
 
 import { MeasureValuesService } from './measure-values.service';
 import { HistoryWindow } from '../shared/history-window/history-window';
 
 @Component({
   templateUrl: './exercise-multicast.ng.html',
-  imports: [HistoryWindow, AsyncPipe, DecimalPipe]
+  imports: [HistoryWindow, AsyncPipe, DecimalPipe, JsonPipe]
 })
 export class ExerciseMulticast implements OnDestroy {
 
@@ -22,7 +22,22 @@ export class ExerciseMulticast implements OnDestroy {
 
   constructor() {
     /**************!!**************/
-    this.measureValues$ = this.#mvs.getValues();
+    // this.measureValues$ = this.#mvs.getValues();
+
+    // 2. multicast (share) the original Observable
+    // this.measureValues$ = this.#mvs.getValues().pipe(
+    //   share()
+    // );
+
+    // 3. multicast (shareReplay) mit Cache
+    this.measureValues$ = this.#mvs.getValues().pipe(
+      shareReplay({
+        refCount: true,
+        bufferSize: 1
+      })
+    );
+
+
     /**************!!**************/
 
   }
