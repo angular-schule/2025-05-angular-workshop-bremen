4bc1e8dd3a77c781f77998ea4ea0d44a9291565c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2025-05-16 16:58:46 +0200

    rxjs: effect

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -5,6 +5,8 @@ import { BookComponent } from "../book/book.component";
 import { BookRatingService } from '../shared/book-rating.service';
 import { BookCreateComponent } from "../book-create/book-create.component";
 import { BookStoreService } from '../shared/book-store.service';
+import { Store } from '@ngrx/store';
+import { selectBooks, selectLoading } from '../store/book.selectors';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,34 +17,33 @@ import { BookStoreService } from '../shared/book-store.service';
 })
 export class DashboardComponent {
 
-  readonly books = signal<Book[]>([]);
-  readonly br = inject(BookRatingService);
-  readonly bs = inject(BookStoreService);
+  books = inject(Store).selectSignal(selectBooks);
+  loading = inject(Store).selectSignal(selectLoading);
+
 
   constructor() {
-
     // this.bs.getAll().subscribe(b => this.books.set(b));
   }
 
   doRateUp(book: Book) {
-    const ratedBook = this.br.rateUp(book);
-    // const ratedBook = {
-    //   ...book,
-    //   rating: Math.min(book.rating + 1, 5)
-    // }
-    this.updateAndSort(ratedBook);
+    // const ratedBook = this.br.rateUp(book);
+    // // const ratedBook = {
+    // //   ...book,
+    // //   rating: Math.min(book.rating + 1, 5)
+    // // }
+    // this.updateAndSort(ratedBook);
   }
 
   doRateDown(book: Book) {
-    const ratedBook = this.br.rateDown(book);
-    this.updateAndSort(ratedBook);
+    // const ratedBook = this.br.rateDown(book);
+    // this.updateAndSort(ratedBook);
   }
 
   updateAndSort(ratedBook: Book) {
 
-    this.books.update(books => books
-      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
-      .sort((a, b) => b.rating - a.rating));
+    // this.books.update(books => books
+    //   .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+    //   .sort((a, b) => b.rating - a.rating));
 
     /*
     const books = this.books();
@@ -56,6 +57,6 @@ export class DashboardComponent {
   }
 
   addBook(newBook: Book) {
-    this.books.update(books => [...books, newBook]);
+    // this.books.update(books => [...books, newBook]);
   }
 }
--- book-rating/src/app/books/store/book.selectors.ts
+++ book-rating/src/app/books/store/book.selectors.ts
@@ -4,3 +4,22 @@ import * as fromBook from './book.reducer';
 export const selectBookState = createFeatureSelector<fromBook.State>(
   fromBook.bookFeatureKey
 );
+
+export const selectBooks = createSelector(
+  selectBookState,
+  state => state.books
+);
+
+export const selectLoading = createSelector(
+  selectBookState,
+  state => state.loading
+);
+
+export function getSpecialBook(title: string) {
+  return createSelector(
+    selectBooks,
+    state => state.find(x => x.title === title)
+  );
+}
+
+export const angularSelector = getSpecialBook('Angular');
